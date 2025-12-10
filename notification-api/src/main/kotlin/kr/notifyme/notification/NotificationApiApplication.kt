83b0bf00d27a1d9f00b79f4df446fe84e9e0803a
han-gyeong <hangyeong@outlook.com>
2025-12-11 00:40:41 +0900

    initial notification-api

---
+++ notification-api/src/main/kotlin/kr/notifyme/notification/NotificationApiApplication.kt
@@ -0,0 +1,11 @@
+package kr.notifyme.notification
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotificationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotificationApiApplication>(*args)
+}
---
+++ notification-api/src/test/kotlin/kr/notifyme/notification/NotificationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.notifyme.notification
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotificationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
