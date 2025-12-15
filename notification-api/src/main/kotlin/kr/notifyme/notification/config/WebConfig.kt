148982737f3d5db3adae33710d56dc0e62c57bff
han-gyeong <hangyeong@outlook.com>
2025-12-15 22:30:30 +0900

    Fix: Test시 운영용 WebConfig 미적용 되도록 추가 [#1]

--- notification-api/src/main/kotlin/kr/notifyme/notification/config/WebConfig.kt
+++ notification-api/src/main/kotlin/kr/notifyme/notification/config/WebConfig.kt
@@ -2,9 +2,11 @@ package kr.notifyme.notification.config
 
 import kr.notifyme.notification.support.auth.CurrentUserIdResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Profile("!test")
 @Configuration
 class WebConfig(
     private val currentUserIdResolver: CurrentUserIdResolver
