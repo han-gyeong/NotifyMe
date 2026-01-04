94f66a47ed365518af3b6b67400360e6080f78b5
han-gyeong <hangyeong@outlook.com>
2026-01-04 16:56:13 +0900

    Config: Auth gradle 하위모듈 설정 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "notifyme"
 include(
     "notification-core",
     "notification-api",
+    "notification-auth",
     "notification-scheduler",
     "notification-sender",
     "notification-gateway"
