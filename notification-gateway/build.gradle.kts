8423e924bb2fdf5471ebd7525cb440c7e2cf6865
han-gyeong <hangyeong@outlook.com>
2026-01-15 00:56:53 +0900

    Feat: notification-gateway 설정 변경 - Eureka 적용 - 프로파일 별 설정 파일 분리

--- notification-gateway/build.gradle.kts
+++ notification-gateway/build.gradle.kts
@@ -24,6 +24,7 @@ extra["springCloudVersion"] = "2025.0.1"
 dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
