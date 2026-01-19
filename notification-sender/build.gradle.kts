fa72fac73704a17023d9f82073f04e028dd44c77
han-gyeong <hangyeong@outlook.com>
2026-01-19 21:29:36 +0900

    Test: Sender 테스트 Properties 상 webhook url 설정 제거

--- notification-sender/build.gradle.kts
+++ notification-sender/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
 	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testImplementation("com.icegreen:greenmail-junit5:2.0.1")
+	testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
