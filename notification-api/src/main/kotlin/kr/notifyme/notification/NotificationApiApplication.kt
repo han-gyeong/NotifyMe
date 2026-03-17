b97e8e58e909f51390d44ca10ff9d2fb8650da2b
han-gyeong <hangyeong@outlook.com>
2026-03-18 00:34:03 +0900

    Feat: EnableAsync 어노테이션 추가

--- notification-api/src/main/kotlin/kr/notifyme/notification/NotificationApiApplication.kt
+++ notification-api/src/main/kotlin/kr/notifyme/notification/NotificationApiApplication.kt
@@ -2,8 +2,10 @@ package kr.notifyme.notification
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableAsync
 @EnableScheduling
 @SpringBootApplication
 class NotificationApiApplication
